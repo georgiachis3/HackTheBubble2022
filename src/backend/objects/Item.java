537d329d98096748149c946ae06388dd1532d94a
Ben Vardy <ben@vardyfamily.co.uk>
2022-10-30 15:12:14 +0000

    Fix json loading

--- src/backend/objects/Item.java
+++ src/backend/objects/Item.java
@@ -1,8 +1,16 @@
 package backend.objects;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Item extends SceneObject {
 
-    public Item(String name, String image, int x, int y) {
+    @JsonCreator
+    public Item(
+            @JsonProperty("name") String name,
+            @JsonProperty("image") String image,
+            @JsonProperty("x") int x,
+            @JsonProperty("y") int y) {
         super(name, image, x, y);
     }
 }
--- src/backend/objects/Portal.java
+++ src/backend/objects/Portal.java
@@ -7,10 +7,12 @@ import java.util.List;
 
 public class Portal {
     private final List<String> toIds;
+    private final List<String> items;
 
     @JsonCreator
-    public Portal(@JsonProperty("to") List<String> to) {
+    public Portal(@JsonProperty("to") List<String> to, @JsonProperty("items") List<String> items) {
         this.toIds = to;
+        this.items = items;
     }
 
     public List<String> getToIds() {
