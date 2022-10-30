3ffa9fa797e8fe5f9d31a80d6accc2b57a8cd098
Ben Vardy <ben@vardyfamily.co.uk>
2022-10-30 17:35:03 +0000

    Add click listener to item

--- src/frontend/JItem.java
+++ src/frontend/JItem.java
@@ -7,24 +7,36 @@ import exceptions.NoSuchGameObjectException;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class JItem extends JButton {
     private final String itemId;
+    private final JFrame frame;
 
-    public JItem(String itemId) throws NoSuchGameObjectException {
+    public JItem(String itemId, JFrame frame) throws NoSuchGameObjectException {
         super("");
         this.itemId = itemId;
+        this.frame = frame;
         Item item = GameObjectStore.getInstance().getItemById(itemId);
 
         ImageIcon icon = new ImageIcon(Game.getResource(item.getImage()));
         setIcon(icon);
 
         Dimension size = getPreferredSize();
-        setBounds(1000,500, size.width, size.height);
+        setBounds(item.getX(),item.getY(), size.width, size.height);
 
         setOpaque(false);
         setContentAreaFilled(false);
         setBorderPainted(false);
+
+        addActionListener(this::clickHandler);
     }
 
+    private void clickHandler(ActionEvent e) {
+        try {
+            Game.getInstance().getPlayer().addItem(itemId);
+        } catch (Exception err) {
+            System.err.println(err.getMessage());
+        }
+    }
 }
