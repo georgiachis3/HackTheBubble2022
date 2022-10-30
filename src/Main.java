c10fca10f6217cfbcb5217e3db8624a0c0978f76
Ben Vardy <ben@vardyfamily.co.uk>
2022-10-30 11:08:46 +0000

    Add main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setSize(400, 500);
+
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+}
